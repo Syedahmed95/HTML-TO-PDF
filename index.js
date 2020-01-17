d7b3b09f7e88f411a10f1452b606dce182907e80
syedahmed95 <syedahmed978@msn.com>
2020-01-17 19:46:07 +0500

    hello

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ var app = express();
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('views/crm.ejs', 'utf8');
-var options = { format: 'a4' };
+var options = { format: 'A4', base: "../public/blocks.jpg"};
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.get("/",function(req,res){
-    res.render("home.ejs");
+    res.render("Challan.ejs");
 });
 
 app.post("/crm",function(req,res){
     console.log(req.body);
     var details = req.body;
     console.log(details)
-    res.render("crm.ejs", { details: details }, function (err, html) {
+    res.render("test.ejs", { details: details }, function (err, html) {
       pdf.create(html, options).toFile('./public/pdf/businesscard.pdf', function (err, res) {
         if (err) {
           console.log(err);
