b4d19384a395846476bb8c983aeac57793dd345b
syedahmed95 <syedahmed978@msn.com>
2020-01-20 03:27:05 +0500

    final

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require("express");
 var app = express();
 //var webshot = require('webshot');
 var fs = require('fs');
+const requestPromise = require('request-promise');
+require('dotenv').config()
 var pdf = require('html-pdf');
 var html = fs.readFileSync('views/crm.ejs', 'utf8');
-var options = { format: 'A4', base: 'file:///'+__dirname +'/public/', orientation: "portrait" };
+var options = { format: 'A4', base: 'file:///'+__dirname +'/public/', orientation: "landscape" };
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -20,7 +22,7 @@ app.post("/crm",function(req,res){
     console.log(req.body);
     var details = req.body;
     console.log(details)
-    res.render("Challan.ejs", { details: details }, function (err, html) {
+    res.render("Challan1.ejs", { details: details }, function (err, html) {
       pdf.create(html, options).toFile('./public/pdf/businesscard.pdf', function (err, res) {
         if (err) {
           console.log(err);
@@ -31,7 +33,23 @@ app.post("/crm",function(req,res){
       res.send(html);
     });
   });
+app.post("/check",()=>{
+  const core_backend_service = {
+    baseurl: process.env.FEE_MODULE_BASE_URL,
+    fetchStudents: {
+        method: 'post',
+        route: '/v1/fee-challan/create"'
+    }
+}
+let challancreate = {
+  method: core_backend_service.fetchStudents.method,
+  uri: core_backend_service.baseurl+core_backend_service.fetchStudents.route,
+  json: true
+};
+ requestPromise(challancreate).then(function(response){console.log(response)}).catch(function(error){console.log(error)});
+// console.log(studentFetchResponse)
 
-app.listen(3000,function(){
+});
+app.listen(4000,function(){
     console.log("Server started successfully!!");
 }); 
